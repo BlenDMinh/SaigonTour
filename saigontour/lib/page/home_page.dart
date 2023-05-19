0ad0e8d9f26e93fed8dfb0cc3215f5376a514002
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-05-19 14:54:49 +0700

    .

--- saigontour/lib/page/home_page.dart
+++ saigontour/lib/page/home_page.dart
@@ -7,7 +7,9 @@ import 'package:saigontour/models/customer.dart';
 import 'package:saigontour/models/payment_method.dart';
 import 'package:saigontour/models/tour_detail.dart';
 import 'package:saigontour/models/user_type.dart';
+import 'package:saigontour/service/tour_service.dart';
 import 'package:saigontour/widget/custom_icon_button.dart';
+import 'package:saigontour/widget/home_page_tour.dart';
 import 'package:saigontour/widget/location_card.dart';
 import 'package:saigontour/widget/nearby_places.dart';
 import 'package:saigontour/widget/recommended_places.dart';
@@ -29,74 +31,91 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: blackBG,
-      appBar: AppBar(
-        // phần đầu của app(tên app, icon tìm kiếm ....)
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        foregroundColor: littleWhite,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 10),
-            const Text(
-              "Chào bạn",
-              style: TextStyle(fontSize: 13),
-            ),
-            Text(
-              "Saigontour",
-              style: TextStyle(
-                  color: whiteColor, fontSize: 25, fontWeight: FontWeight.w800),
-            ),
-          ],
-        ),
-      ),
-      body: ListView(
-        // list này là toàn bộ phần dưới phần appbar
-        physics:
-            const BouncingScrollPhysics(), // kéo xuống có hiệu hứng bouncing
-        padding: const EdgeInsets.all(14),
-        children: [
-          // LOCATION CARD để lấy địa chỉ hiện tại của user
-          const LocationCard(),
-          const SizedBox(
-            height: 15,
-          ),
-          //TouristPlaces nhấn vào để show ra các địa điểm du lịch theo sở thích(Núi, rừng, biển...) (Chưa làm)
-          //const TouristPlaces(),
-          // CATEGORIES Recommendation
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Gợi ý địa điểm ",
-                style: TextStyle(
-                    color: white, fontSize: 22, fontWeight: FontWeight.bold),
+    var service = TourService();
+    return FutureBuilder<List<TourModel>>(
+        future: service.getAll(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<TourModel>> snapshot) {
+          return Scaffold(
+            backgroundColor: blackBG,
+            appBar: AppBar(
+              // phần đầu của app(tên app, icon tìm kiếm ....)
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              foregroundColor: littleWhite,
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  const Text(
+                    "Chào bạn",
+                    style: TextStyle(fontSize: 13),
+                  ),
+                  Text(
+                    "Saigontour",
+                    style: TextStyle(
+                        color: whiteColor,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ],
               ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          //Widget này là thanh slide để recommend các nơi nên đến (nằm trong folder widget)
-          const RecommendedPlaces(),
-          const SizedBox(height: 10),
-          // Phần này để show các địa điểm du lịch gần với user dựa vào location đã lấy từ LOCATION CARD
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Gần bạn",
-                style: TextStyle(
-                    color: white, fontSize: 22, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          //nằm trong folder widget
-          const NearbyPlaces(),
-        ],
-      ),
-    );
+            ),
+            body: ListView(
+              // list này là toàn bộ phần dưới phần appbar
+              physics:
+                  const BouncingScrollPhysics(), // kéo xuống có hiệu hứng bouncing
+              padding: const EdgeInsets.all(14),
+              children: [
+                // LOCATION CARD để lấy địa chỉ hiện tại của user
+                const LocationCard(),
+                const SizedBox(
+                  height: 15,
+                ),
+                //TouristPlaces nhấn vào để show ra các địa điểm du lịch theo sở thích(Núi, rừng, biển...) (Chưa làm)
+                //const TouristPlaces(),
+                // CATEGORIES Recommendation
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Gợi ý địa điểm ",
+                      style: TextStyle(
+                          color: white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                //Widget này là thanh slide để recommend các nơi nên đến (nằm trong folder widget)
+                const RecommendedPlaces(),
+                const SizedBox(height: 10),
+                // Phần này để show các địa điểm du lịch gần với user dựa vào location đã lấy từ LOCATION CARD
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Tất cả",
+                      style: TextStyle(
+                          color: white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Column(
+                  children: List.generate(
+                      snapshot.data == null ? 0 : snapshot.data!.length,
+                      (index) => HomeTourCart(context, snapshot.data![index])),
+                )
+                //nằm trong folder widget
+                // const NearbyPlaces(),
+              ],
+            ),
+          );
+        });
   }
 }
---
+++ saigontour/lib/widget/home_page_tour.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:saigontour/models/tour_model.dart';
+
+import '../consts/colors.dart';
+import '../models/nearby_places_model.dart';
+import '../page/tourist_details_page.dart';
+import 'distance.dart';
+
+Widget HomeTourCart(BuildContext context, TourModel tour) {
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 10),
+    child: SizedBox(
+      height: 135,
+      width: 360,
+      child: Card(
+        color: blackTextFild,
+        elevation: 0.4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(12),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TouristDetailsPage(
+                    image: nearbyPlaces[0].image,
+                  ),
+                ));
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    nearbyPlaces[0].image,
+                    height: double.maxFinite,
+                    width: 120,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tour.name!,
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: littleWhite),
+                      ),
+                      Text(
+                        tour.description!,
+                        style: TextStyle(fontSize: 16, color: littleWhite),
+                      ),
+                      const SizedBox(height: 10),
+                      // DISTANCE WIDGET
+                      const Distance(),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow.shade700,
+                            size: 14,
+                          ),
+                          const Spacer(),
+                          RichText(
+                            text: TextSpan(
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: purpButton,
+                                ),
+                                text: tour.price.toString(),
+                                children: const [
+                                  TextSpan(
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: purpButton,
+                                      ),
+                                      text: "/ Người")
+                                ]),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
